220ecb6f4ea5b7340e3d286ef8ac7a185e4f9129
ayo baj <ayobaj007@gmail.com>
2024-04-16 15:40:07 +0100

    navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import NavBar from "./components/NavBar";
+
+
+const App = () => {
+  return (
+    <div className="">
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import { IoSearchSharp } from "react-icons/io5";
+
+const NavBar = () => {
+    return (
+    <div className="bg-slate-200 shadow-md ">
+
+        <div className="flex justify-between max-w-6xl mx-auto p-3 items-center">
+
+            <Link to="/">
+            <h1 className="font-bold text-sm sm:text-xl flex-wrap">
+                <span className="text-purple-800">Wesam</span>
+                <span className="text-purple-400">Yet</span>
+            </h1>
+            </Link>
+
+            <form className="bg-slate-100 p-3 rounded-md flex items-center">
+
+                <input type="text" className="bg-transparent focus:outline-none w-24 sm:w-64" placeholder="Search..."/>
+                <IoSearchSharp className="text-black"/>
+
+            </form>
+
+            <ul className=" flex space-x-4">
+
+                <Link to="/">
+                <li className="hidden sm:inline text-purple-400 font-bold hover:underline">Home</li>
+                </Link>
+
+                <Link to="/about">
+                <li className="hidden sm:inline text-purple-400 font-bold hover:underline">About</li>
+                </Link>
+
+                <Link to="/sign-in">
+                <li className=" sm:inline text-purple-400 font-bold hover:underline">{''}Sign in</li>
+                </Link>
+
+            </ul>
+
+        </div>
+
+    </div>
+    )
+}
+
+export default NavBar
