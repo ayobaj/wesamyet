07ab04849d1c28c4fccb2bedc965c7fd9c2f7a19
ayo baj <ayobaj007@gmail.com>
2024-05-18 15:04:28 +0100

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy: {
-      '/backend':{
-        target: import.meta.env.VITE_ENVIRONMENT,
-        secure: false,
-      }
-    }
-  },
   plugins: [react()],
 })
